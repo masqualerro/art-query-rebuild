86ed84912bfdd3bdb79158cd4f06c2ea337a4ce8
masqualerro <86025634+masqualerro@users.noreply.github.com>
2024-03-26 10:10:21 -0700

    edit readme, user name on insights

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# art-query-rebuild
- 
+# Art Query Rebuild :art:
+
+## About
+
+This project is a rebuild of one of my first projects, art-query. The original project was built using vanilla JavaScript, HTML, and CSS. This project is built using Vue and TailwindCSS. :rocket:
+
+I have built an API with NestJs, MySQL, and TypeORM to serve the data for this project. The API and DB are hosted on Digital Ocean and the client is hosted on Vercel.
+
+The API is available here: [Art Query API](https://github.com/masqualerro/art-query-api)
+
+### Exploring Artworks
+This app serves as a platform for exploring a diverse range of artworks and gaining insights from your favorites.
+
+### Discovering Artworks
+Use the search input in the main navbar to find artworks that capture your interest. You can specify which art API to search from using the dropdown menu. Navigate to 'Chicago Art Institute' or 'Harvard Art Museums' to view an unfiltered display of art from each respective museum. Save your favorite pieces to your collection by hovering over them and clicking the heart icon.
+
+### Your Collection
+Explore your saved collection of art and gain insights into your preferences, such as color palettes and cultural themes, on the "Collection" page.
+
+### Data Sources
+This app utilizes data from the Harvard Art Museums and the Art Institute of Chicago APIs.
+
+For more information, visit the respective API documentation:
+
+[Art Institute of Chicago API](https://api.artic.edu/docs/)
+
+[Harvard Art Museums API](https://github.com/harvardartmuseums/api-docs)
+
+## Installation
+
+```bash
+npm install
+npm run dev
--- src/views/collection/CollectionInsights.vue
+++ src/views/collection/CollectionInsights.vue
@@ -75,19 +75,14 @@
             <div class="flex items-center gap-x-6">
               <BlackGlyph class="h-16 w-16 flex-none rounded-full ring-1 ring-gray-900/10 p-3" />
               <h1>
-                <div class="text-sm leading-6 text-gray-500">Miguel Sedillo</div>
-                <div class="mt-1 text-base font-semibold leading-6 text-gray-900">
+                <span class="text-sm leading-6 text-gray-500">{{userStore.user?.name}}</span>
+                <br>
+                <span class="mt-1 text-base font-semibold leading-6 text-gray-900">
                   Artwork Insights
-                </div>
+                </span>
               </h1>
             </div>
             <div class="flex items-center gap-x-4 sm:gap-x-6">
-              <!-- <a
-                href="#"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >Download</a
-              > -->
-
               <Menu as="div" class="relative sm:hidden">
                 <MenuButton class="-m-3 block p-3">
                   <span class="sr-only">More</span>
@@ -302,28 +297,8 @@
     </div>
   </main>
 </template>
-<style scoped>
-.slide-fade-enter-active {
-  transition: all 0.3s ease;
-}
-.slide-fade-leave-active {
-  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
-}
-.slide-fade-enter {
-  width: 0;
-  opacity: 0;
-}
-.slide-fade-leave-to {
-  width: 100%;
-  opacity: 1;
-}
-.color-button:hover {
-  filter: brightness(120%);
-  opacity: 0.8;
-}
-</style>
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed } from 'vue'
 import axios from 'axios'
 import { useUserStore } from '@/stores/user'
 import type { colorResponse } from '@/interfaces/insights.interfaces'
@@ -339,54 +314,13 @@ import {
 } from '@headlessui/vue'
 import { EllipsisVerticalIcon, ChevronDownIcon } from '@heroicons/vue/20/solid'
 import BlackGlyph from '@/components/icons/BlackGlyph.vue'
-// import colorInsightData from './mockData.js/colorInsight'
 import ColorEChart from '@/components/ColorEChart.vue'
 import type { cultureInsight } from '@/interfaces/insights.interfaces'
 import countries from '../../../node_modules/flag-icons/country.json'
 import nationalitiesCountries from '@/json/nationalitiesCountries.json'
 import type { styleInsight } from '@/interfaces/insights.interfaces'
 import type { artistInsight } from '@/interfaces/insights.interfaces'
-import { color } from 'echarts'
 
-const invoice = {
-  subTotal: '$8,800.00',
-  tax: '$1,760.00',
-  total: '$10,560.00',
-  items: [
-    {
-      id: 1,
-      title: 'Logo redesign',
-      description: 'New logo and digital asset playbook.',
-      hours: '20.0',
-      rate: '$100.00',
-      price: '$2,000.00'
-    },
-    {
-      id: 2,
-      title: 'Website redesign',
-      description: 'Design and program new company website.',
-      hours: '52.0',
-      rate: '$100.00',
-      price: '$5,200.00'
-    },
-    {
-      id: 3,
-      title: 'Business cards',
-      description: 'Design and production of 3.5" x 2.0" business cards.',
-      hours: '12.0',
-      rate: '$100.00',
-      price: '$1,200.00'
-    },
-    {
-      id: 4,
-      title: 'T-shirt design',
-      description: 'Three t-shirt design concepts.',
-      hours: '4.0',
-      rate: '$100.00',
-      price: '$400.00'
-    }
-  ]
-}
 const tabs = [
   { name: 'Color', current: true },
   { name: 'Culture', current: false },
@@ -396,7 +330,6 @@ const tabs = [
 const api = import.meta.env.VITE_APP_API
 const userStore = useUserStore()
 const loading = ref(true)
-// const colorData = ref<colorResponse>()
 const colorData = ref<colorResponse>()
 const cultureData = ref<cultureInsight[]>()
 const styleData = ref<styleInsight[]>()
@@ -486,7 +419,6 @@ function findCountryByNationality(nationality: string) {
 
   let country = countries.find((c) => c.name === nationalityObject?.en_short_name)
 
-  // Return the country code instead of the whole country object
   return country ? country.code : null
 }
 
